c44bdb164e61c9e9818083397db790b215598451
Evgeny <evgprojects@gmail.com>
2021-05-19 21:42:41 +0300

    colors update

--- motorcycles/contactUs.js
+++ motorcycles/contactUs.js
@@ -1,5 +1,5 @@
 function submitMessage() {
-  const comment = document.getElementById("form");
+  const comment = document.getElementById("box-2");
   let firstName = document.querySelector("#firstName").value;
   let secondName = document.querySelector("#secondName").value;
   // let backToTheMainPage = document.createElement("a");
@@ -8,7 +8,8 @@ function submitMessage() {
   comment.innerHTML = `Hi ${firstName} ${secondName},
    Thank you for contacting us, we will call you soon.`;
   comment.style.width = "fit-content";
-  comment.style.backgroundColor = "darkkhaki";
+  comment.style.backgroundColor = "rgb(34, 169, 199)";
+  comment.style.color = "white";
   //comment.appendChild(backToTheMainPage);
   window.setTimeout(function () {
     window.location.href = "index.html";
